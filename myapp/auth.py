fe9674ba571e82bed0c8b189a40cfdcd7e4e1ea7
kaestro <didme654@snu.ac.kr>
2024-02-09 11:13:58 +0900

    email을 헤더로 받은 뒤, 관련된 세션 반환하는 함수 작성(TODO included)
    TODO
    
    이전에 세션을 받은 부분들을 해당 메소드를 사용하도록 리팩토링

--- myapp/auth.py
+++ myapp/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Header
 from .cache.cache_manager import CacheManager
+from typing import Tuple
 
 # 해당 요청이 들어올 경우, 세션 ID를 헤더에서 가져온다.
 def get_session_id(session_id: str = Header(None)):
@@ -8,8 +9,6 @@ def get_session_id(session_id: str = Header(None)):
         raise HTTPException(status_code=401, detail="Session ID not found")
     return session_id
 
-# 세션 id에 맞춰 email을 반환한다.
-# 성공적으로 반환하면 세션 만료 시간을 연장한다.
 def get_current_user_email(session_id: str = Depends(get_session_id)) -> str:
     cache_manager = CacheManager()
     user_email = cache_manager.get(session_id)
@@ -19,3 +18,23 @@ def get_current_user_email(session_id: str = Depends(get_session_id)) -> str:
     cache_manager.extend_session(session_id)    
 
     return user_email
+
+
+# email을 헤더에서 받아온 이후에, 데이터베이스에서 해당 email에 해당하는
+# 유저의 세션과 id를 가져온다.
+def get_user_session_and_id(user_email: str, DatabaseManager, User) -> Tuple[Session, str, int]:
+    # 세션 확인
+    if user_email is None:
+        raise HTTPException(status_code=401, detail="User not logged in")
+
+    # 데이터베이스 세션 가져오기
+    session = DatabaseManager().get_session()
+    user_email = user_email.decode("utf-8")
+    user = session.query(User).filter_by(email=user_email).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_id = user.id
+
+    return session, user_email, user_id
