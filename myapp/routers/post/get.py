1272126ab29cea9c2977869d725ace6552c64f8e
kaestro <didme654@snu.ac.kr>
2024-02-10 08:10:43 +0900

    Minor refactoring on /post/get
    Specified the http exception message to notify board missing situation

--- myapp/routers/post/get.py
+++ myapp/routers/post/get.py
@@ -16,7 +16,9 @@ def get_post(post_id: int, user_email: str = Depends(get_current_user_email)):
 
     # 본인이 생성하거나, 전체 공개된 게시판의 게시글인지 확인
     target_post = database_session.query(Post).filter_by(id=post_id).first()
-    if target_post is None or (target_post.user_id != user_id and not target_post.board.public):
+    if target_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    elif target_post.user_id != user_id and not target_post.board.public:
         raise HTTPException(status_code=403, detail="Cannot get post not owned by user and not public")
 
     # 게시글 조회
